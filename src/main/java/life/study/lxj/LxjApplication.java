5068a1ecf663dbb35ca7f8c8c239f56c8d7d517b
LebronL <1932707397@qq.com>
2020-07-16 14:02:18 +0800

    init repo

---
+++ src/main/java/life/study/lxj/LxjApplication.java
@@ -0,0 +1,13 @@
+package life.study.lxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/study/lxj/LxjApplicationTests.java
@@ -0,0 +1,13 @@
+package life.study.lxj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
