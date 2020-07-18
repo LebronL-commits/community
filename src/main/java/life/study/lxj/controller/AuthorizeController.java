66a699853c0fad7fe0a7b4cc56cc95e97de1f0e8
LebronL <1932707397@qq.com>
2020-07-18 18:40:33 +0800

    keep sign in

--- src/main/java/life/study/lxj/controller/AuthorizeController.java
+++ src/main/java/life/study/lxj/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -27,13 +29,14 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.Redirect_uri}")
     private String Redirect_url;
-    @Autowired
+    @Autowired(required = false)
     private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state,
-                            HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(Redirect_url);
@@ -44,15 +47,15 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if(githubUser!=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setAccount_id(String.valueOf(githubUser.getId()));
             user.setName(githubUser.getName());
             user.setGmtcreate(System.currentTimeMillis());
-            user.setGmtmodified(user.getGmtmodified());
+            user.setGmtmodified(user.getGmtcreate());
             System.out.println(githubUser.getName());
             userMapper.insert(user);
-            //登陆成功，写cookie和session
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             return "redirect:/";
--- src/main/java/life/study/lxj/mapper/UserMapper.java
+++ src/main/java/life/study/lxj/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package life.study.lxj.mapper;
 import life.study.lxj.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{account_id},#{name},#{token},#{gmtcreate},#{gmtmodified})")
     void insert(User user);
-
+    @Select("select * from user where token = #{token}")
+    User findbytoken(@Param("token") String token);
 }
